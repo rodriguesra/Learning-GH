4727a52992cc21881d588f8c4d0c649578e9f250
rodriguesra <rodriguesra@hotmail.com>
2021-05-30 17:52:36 -0300

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Rafael Rodrigues on 30/05/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
